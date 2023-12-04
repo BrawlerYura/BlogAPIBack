c5aa81b9cef97c7615c75379e6aac89d0122b090
BrawlerYura <sitdikov.yuriy@gmail.com>
2023-12-04 11:39:40 +0700

    Прописал ДТО
    Прописаны все ДТО из сваггера

---
+++ BlogApi/BlogApi/DTO/CommunityUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogApi.DTO;
+
+public class CommunityUserDto
+{
+    public Guid UserId { get; set; }
+    public Guid CommunityId { get; set; }
+    public CommunityRole Role { get; set; }
+}
