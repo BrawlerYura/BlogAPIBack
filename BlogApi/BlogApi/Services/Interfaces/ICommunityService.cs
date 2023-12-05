6bbe181ff68242addb658687828ea21d56b87e19
BrawlerYura <sitdikov.yuriy@gmail.com>
2023-12-05 23:59:32 +0700

    запрсы community
    все кроме communityService

---
+++ BlogApi/BlogApi/Services/Interfaces/ICommunityService.cs
@@ -0,0 +1,24 @@
+﻿using BlogApi.Data.Models;
+using BlogApi.DTO;
+
+namespace BlogApi.Services.Interfaces;
+
+public interface ICommunityService
+{
+    Task<List<CommunityDto>> GetCommunityList();
+    
+    Task<List<CommunityUserDto>> GetMyCommunityList();
+
+    Task<CommunityFullDto> GetCommunity(Guid communityId);
+
+    Task<PostPagedListDto> GetCommunityPostList(Guid communityId, List<TagDto> tags, PostSorting sorting, int page,
+        int size);
+
+    Task<Guid> CreatePost(Guid communityId, CreatePostDto createPostDto);
+
+    Task<CommunityRole?> GetCommunityRole(Guid communityId);
+
+    Task SubscribeToCommunity(Guid communityId);
+    
+    Task UnsubscribeFromCommunity(Guid communityId);
+}
